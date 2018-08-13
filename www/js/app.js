70d0c67673eb4444036239be5bbf5d29100f2c36
Yanlin Sun <ysun11@ad.emich.edu>
2018-08-13 15:02:36 -0400

    test

--- www/js/app.js
+++ www/js/app.js
@@ -45,7 +45,7 @@ angular.module('starter', ['ionic','ngCordova'])
         });
     } else {
         if (ionic.Platform.isIOS() || ionic.Platform.isIPad()) {
-            appId = 'bblearn://';
+            appId = 'blackboard://';
            // appId = '950424861://';
             appStarter = startApp.set(appId);
             appStarter.start(function(msg) {
