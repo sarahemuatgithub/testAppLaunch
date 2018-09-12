cc7f006dedbee3a18096c573a582dd8c4f7a8466
Yanlin Sun <ysun11@ad.emich.edu>
2018-09-12 08:31:55 -0400

    test

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener('deviceready', function() {
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngCordova','ui.router'])
+angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -92,13 +92,4 @@ angular.module('starter', ['ionic','ngCordova','ui.router'])
       );      
   } */ 
   
-  .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
-
-    $stateProvider
-    .state('appLaunch', {
-            url: '/appLaunch',
-            templateUrl: 'applaunch.html',
-            controller:"CheckController" 
-        })
-      })
 });
